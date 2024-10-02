2e38932cbbcebfdaeaec8bfe73aa529c740a748a
Jamie430wwwww <jamie430w@gmail.com>
2024-10-01 22:03:14 -0400

    DONE!!!!!

--- playlist/script.js
+++ playlist/script.js
@@ -8,15 +8,16 @@ const canvasContext = visualizerCanvas.getContext('2d');
 const song1btn = document.getElementById('青春コンプレックス');
 const song2btn = document.getElementById('Distortion!!');
 const song3btn = document.getElementById('カラカラ');
-const song4btn = document.getElementById('');
-const song5btn = document.getElementById('');
-const song6btn = document.getElementById('');
-const song7btn = document.getElementById('');
-const song8btn = document.getElementById('');
-const song9btn = document.getElementById('');
-const song10btn = document.getElementById('');
+const song4btn = document.getElementById('ギターと孤独と蒼い惑星');
+const song5btn = document.getElementById('あのバンド');
+const song6btn = document.getElementById('忘れてやらない');
+const song7btn = document.getElementById('星座になれたら');
+const song8btn = document.getElementById('Lemon');
+const song9btn = document.getElementById('転がる岩、君に朝が降る');
+const song10btn = document.getElementById('Re:Re:');
 var Cover = document.getElementById('Cover');
 var PlayingSong = document.getElementById('playingSong');
+var bandname = document.getElementById('band-name');
 
 let audioContext, analyser, dataArray, bufferLength;
 
@@ -142,6 +143,7 @@ song1btn.addEventListener('click', ()=>{
     audioPlayer = document.getElementById("audio-player1");
     Cover.src = "https://i.ebayimg.com/images/g/hOIAAOSwYZxjbkFJ/s-l400.jpg";
     PlayingSong.textContent = '青春コンプレックス';
+    bandname.textContent = '結束バンド'
     resetTimeBar();
 })
 song2btn.addEventListener('click', ()=>{
@@ -150,6 +152,7 @@ song2btn.addEventListener('click', ()=>{
     audioPlayer = document.getElementById("audio-player2");
     Cover.src = "https://static.wikia.nocookie.net/bocchi-the-rock/images/3/35/Distortion%21%21_Digital_Cover_%28ANXX-01128%29.png/revision/latest?cb=20230127092411";
     PlayingSong.textContent = 'Distortion!!';
+    bandname.textContent = '結束バンド'
     resetTimeBar();
 })
 song3btn.addEventListener('click', ()=>{
@@ -158,40 +161,69 @@ song3btn.addEventListener('click', ()=>{
     audioPlayer = document.getElementById("audio-player3");
     Cover.src = "https://static.wikia.nocookie.net/bocchi-the-rock/images/7/72/Karakara_Digital_Cover_%28ANXX-01131%29.png/revision/latest?cb=20230127092506";
     PlayingSong.textContent = 'カラカラ';
+    bandname.textContent = '結束バンド'
     resetTimeBar();
 })
 song4btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player4");
+    Cover.src = "https://static.wikia.nocookie.net/bocchi-the-rock/images/e/eb/Guitar_to_Kodoku_to_Aoihoshi_Digital_Cover_%28ANXX-01133%29.png/revision/latest?cb=20230127092618"
+    PlayingSong.textContent = 'ギターと孤独と蒼い惑星';
+    bandname.textContent = '結束バンド'
+    resetTimeBar();
 })
 song5btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player5");
+    Cover.src = "https://static.wikia.nocookie.net/bocchi-the-rock/images/9/94/Ano_Band_Digital_Cover_%28ANXX-01134%29.png/revision/latest?cb=20230127092800";
+    PlayingSong.textContent = 'あのバンド';
+    bandname.textContent = '結束バンド'
+    resetTimeBar();
 })
 song6btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player6");
+    Cover.src = "https://static.wikia.nocookie.net/bocchi-the-rock/images/a/ac/Wasurete_Yaranai_Digital_Cover_%28ANXX-01155%29.png/revision/latest?cb=20230127092851"
+    PlayingSong.textContent = '忘れてやらない';
+    bandname.textContent = '結束バンド'
+    resetTimeBar();
 })
 song7btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player7");
+    Cover.src = "https://images.genius.com/2a742e12eb71856bc273e3777130696b.1000x1000x1.png"
+    PlayingSong.textContent = '星座になれたら';
+    bandname.textContent = '結束バンド'
+    resetTimeBar();
 })
 song8btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player8");
+    Cover.src = 'https://upload.wikimedia.org/wikipedia/en/1/12/Kenshi_Yonezu_-_Lemon.png'
+    PlayingSong.textContent = 'Lemon';
+    bandname.textContent = '米津 玄師'
+    resetTimeBar();
 })
 song9btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player9");
+    Cover.src = 'https://static.wikia.nocookie.net/bocchi-the-rock/images/0/0c/Korogaru_Iwa%2C_Kimi_ni_Asa_ga_Furu_Digital_Cover_%28ANXX-01156%29.png/revision/latest/thumbnail/width/360/height/360?cb=20230127093406'
+    PlayingSong.textContent = '転がる岩、君に朝が降る';
+    bandname.textContent = '結束バンド'
+    resetTimeBar();
 })
 song10btn.addEventListener('click', ()=>{
     audioPlayer.pause();
     console.log("test");
     audioPlayer = document.getElementById("audio-player10");
+    Cover.src = 'https://upload.wikimedia.org/wikipedia/en/6/69/Re_Re_Asian_Kung-Fu_Generation.jpg';
+    PlayingSong.textContent = 'Re:Re:';
+    bandname.textContent = 'Asian Kung-Fu Generation'
+    resetTimeBar();
 })
